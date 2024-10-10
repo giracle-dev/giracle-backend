e7fbc8df0231fbbe44418009f7ce88a63574ba4f
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-11 05:13:47 +0900

    [add] - ロールの権限確認とレベル計算処理（土台）のミドルウェア作成

--- src/Middlewares.ts
+++ src/Middlewares.ts
@@ -36,4 +36,62 @@ const CheckToken = new Elysia({ name: 'CheckToken' })
   }
 );
 
+const compareRoleLevelToRole = new Elysia({ name: 'compareRoleLevelToRole' })
+  .use(CheckToken)
+  .macro(
+    ({onBeforeHandle}) => ({
+      async compareRoleLevelToRole(dat:{targetRoleId: string}, { _userId }) {
+        //ユーザーとロール情報取得
+        const userWithRoles = await db.user.findUnique({
+          where: {
+            id: _userId
+          },
+          include: {
+            RoleLink: true
+          }
+        });
+
+        const role = await db.roleInfo.findUnique({
+          where: {
+            id: dat.targetRoleId
+          }
+        });
+
+        if (role === null) {
+          return error(404, "Role not found");
+        }
+
+        console.log("compareRoleLevelToRole :: triggered");
+      }
+    })
+  );
+
+const checkRoleTerm = new Elysia({ name: 'checkRoleTerm' })
+  .use(CheckToken)
+  .macro(
+    ({ onBeforeHandle }) => ({
+    async checkRoleTerm(roleTerm: string) {
+      onBeforeHandle(async ({ _userId }) => {
+        console.log("送信者のロールId->", _userId);
+
+        //該当権限を持つロール付与情報を検索
+        const roleLink = await db.roleLink.findFirst({
+          where: {
+            userId: _userId,
+            role: {
+              manageRole: true
+            }
+          }
+        });
+
+        //該当権限を持つロール付与情報が無いなら停止
+        if (roleLink === null) {
+          return error(401, "Role level not enough");
+        }
+      });
+    }
+    })
+  );
+
 export default CheckToken;
+export { compareRoleLevelToRole, checkRoleTerm };
