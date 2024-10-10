9f21289c1c275bafaad636e053a2f2ef322b2fc0
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-11 02:04:16 +0900

    [add] - ミドルウェアにデータを渡せるタイプのトークン確認機能追加

--- src/Middlewares.ts
+++ src/Middlewares.ts
@@ -1,10 +1,32 @@
 import { Elysia, error } from 'elysia'
 import { PrismaClient } from "@prisma/client";
-import { errorToResponse } from 'elysia/dist/handler';
 
 const db = new PrismaClient();
 
 const Middlewares = new Elysia({ name: 'CheckToken' })
+  .derive({ as: "scoped"}, async ({ cookie: { token } }) => {
+    //クッキーが無いなら停止
+    if (token.value === undefined) {
+      return {
+        userId: ""
+      }
+    }
+
+    //トークンがDBにあるか確認
+    const tokenData = await db.token.findUnique({
+      where: {
+        token: token.value
+      }
+    });
+    //トークンが無いなら停止
+    if (tokenData === null) {
+      throw error(401, "Token is invalid");
+    }
+
+    return {
+      userId: tokenData.userId
+    }
+  })
   .macro(({ onBeforeHandle }) => ({
     logToken() {
       onBeforeHandle((context) => {
@@ -13,10 +35,10 @@ const Middlewares = new Elysia({ name: 'CheckToken' })
     },
 
     checkToken() {
-      onBeforeHandle(async (context) => {
+      onBeforeHandle(async ({cookie, userId}) => {
         //console.log("CheckToken :: checkToken : cookie->", context.cookie);
 
-        const tokenValue = context.cookie.token.value;
+        const tokenValue = cookie.token.value;
         if (tokenValue === undefined) {
           throw error(401, "Token is invalid");
         }
