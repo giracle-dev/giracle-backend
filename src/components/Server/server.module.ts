16e2df5e51e91f05091f2e47e501f13cfd15c82b
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-18 15:17:18 +0900

    [add] - サーバー設定データの取得と更新ハンドラ作成

---
+++ src/components/Server/server.module.ts
@@ -0,0 +1,51 @@
+import Elysia, { error, t } from "elysia";
+import CheckToken, { checkRoleTerm, urlPreviewControl } from "../../Middlewares";
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+export const message = new Elysia({ prefix: "/server" })
+  .get(
+    "/config",
+    async () => {
+      const config = await db.serverConfig.findFirst();
+      return {
+        data: {
+          config,
+        },
+      };
+    },
+    {
+      detail: {
+        description: "サーバーの設定を取得します",
+        tags: ["Server"],
+      },
+    }
+  )
+  .use(CheckToken)
+  .use(checkRoleTerm)
+  .post(
+    "/change-info",
+    async ({ body: {name, introduction} }) => {
+      await db.serverConfig.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          name,
+          introduction,
+        },
+      });
+    },
+    {
+      body: t.Object({
+        name: t.String({ minLength: 1 }),
+        introduction: t.String({ minLength: 1 }),
+      }),
+      detail: {
+        description: "サーバーの設定を変更します",
+        tags: ["Server"],
+      },
+      checkRoleTerm: "manageServer"
+    }
+  )
