20de46f5f2d0b6135cdd0e6de1e5fe785c8362a5
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-19 18:56:29 +0900

    History end or top (#28)
    * [change] - 履歴取得時に履歴の最新か最古かを取得できるように
    
    * [fix] - 履歴取得方向を考慮して最古、最新を判別
    
    * [change] - 履歴取得のテストを更新
    
    * Biome Check

--- src/components/Channel/channel.module.ts
+++ src/components/Channel/channel.module.ts
@@ -253,9 +253,47 @@ export const channel = new Elysia({ prefix: "/channel" })
         orderBy: { createdAt: "desc" },
       });
 
+      //履歴の最新まで取ったかどうかを判別するために最初のメッセージを取得
+      const firstMessageOfChannel = await db.message.findFirst({
+        where: {
+          channelId: channelId,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      //取得した履歴が最新まで取得したか、または最初まで取得したかを判別
+      let atEnd = false;
+      let atTop = false;
+      //取得方向によって判別方法が異なる
+      if (fetchDirection === "newer") {
+        //取得した履歴がある場合
+        if (history[0] !== undefined) {
+          atTop = firstMessageOfChannel?.id === history[0].id;
+        } else {
+          //取得した履歴がない場合、最初まで取得したと判定
+          atTop = true;
+        }
+        atEnd = history.length < (fetchLength || 30);
+      } else {
+        //取得した履歴がある場合
+        if (history[0] !== undefined) {
+          atEnd = firstMessageOfChannel?.id === history[0].id;
+        } else {
+          //取得した履歴がない場合、最初まで取得したと判定
+          atEnd = true;
+        }
+        atTop = history.length < (fetchLength || 30);
+      }
+
       return {
         message: "History fetched",
-        data: history,
+        data: {
+          history,
+          atEnd: firstMessageOfChannel?.id === history[0].id,
+          atTop: history.length < (fetchLength || 30),
+        },
       };
     },
     {
