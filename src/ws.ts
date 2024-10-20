74fdb8c348deb7eddd69d758cbeb8be8adb1dfe9
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-20 17:40:59 +0900

    [add] - WS接続の接続と切断でオンラインユーザーの加減を通知するように

--- src/ws.ts
+++ src/ws.ts
@@ -3,7 +3,7 @@ import Elysia, { t } from "elysia";
 import type { ElysiaWS } from "elysia/dist/ws";
 
 const db = new PrismaClient();
-//ユーザーごとのWSインスタンス管理
+//ユーザーごとのWSインスタンス管理 ( Map <UserId, WSインスタンス>)
 // biome-ignore lint/suspicious/noExplicitAny: どのwsインスタンスでも受け付けるためにany
 export const userWSInstance = new Map<string, ElysiaWS<any, any, any>>();
 
@@ -64,11 +64,46 @@ export const wsHandler = new Elysia().ws("/ws", {
 
     //このユーザーWSインスタンス保存
     userWSInstance.set(user.id, ws);
+    //ユーザー接続通知
+    ws.publish("GLOBAL", JSON.stringify({
+      signal: "user::Connected",
+      data: user.id,
+    }));
 
     console.log("index :: 新しいWS接続");
   },
 
-  close(ws) {
+  async close(ws) {
     console.log("ws :: WS切断");
+
+    //トークンを取得して有効か調べる
+    const token = ws.data.cookie.token.value || ws.data.query.token;
+    if (!token) {
+      return;
+    }
+
+    const user = await db.user.findFirst({
+      where: {
+        Token: {
+          some: {
+            token: token,
+          },
+        },
+      },
+      include: {
+        ChannelJoin: true,
+      },
+    });
+    if (!user) {
+      return;
+    }
+
+    //このユーザーWSインスタンス削除
+    userWSInstance.delete(user.id);
+    //ユーザー接続通知
+    ws.publish("GLOBAL", JSON.stringify({
+      signal: "user::Disconnected",
+      data: user.id,
+    }));
   },
 });
