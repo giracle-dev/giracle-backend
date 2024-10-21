ac9440ef8443ab2c910686df759904e42044d75b
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-21 17:23:34 +0900

    Role list (#42)
    * [add] - ロール一覧の取得ハンドラ作成
    
    * [change] - 変数名更新
    
    * [add] - ロール情報更新用ハンドラ作成

--- src/components/Role/role.module.ts
+++ src/components/Role/role.module.ts
@@ -45,6 +45,48 @@ export const role = new Elysia({ prefix: "/role" })
     },
   )
   .post(
+    "/update",
+    async ({ body: { roleId, roleData }, _userId }) => {
+      if (roleId === "HOST") throw error(400, "You cannot update HOST role");
+
+      const roleUpdated = await db.roleInfo.update({
+        where: {
+          id: roleId
+        },
+        data: {
+          createdUserId: _userId,
+          ...roleData,
+        },
+      });
+
+      return {
+        success: true,
+        message: "Role updated",
+        data: {
+          roleId: roleUpdated.id,
+        },
+      };
+    },
+    {
+      body: t.Object({
+        roleId: t.String(),
+        roleData: t.Object({
+          name: t.String(),
+          color: t.String(),
+          manageServer: t.Optional(t.Boolean()),
+          manageChannel: t.Optional(t.Boolean()),
+          manageRole: t.Optional(t.Boolean()),
+          manageUser: t.Optional(t.Boolean()),
+        }),
+      }),
+      detail: {
+        description: "ロール情報を更新します",
+        tags: ["Role"],
+      },
+      checkRoleTerm: "manageRole",
+    },
+  )
+  .post(
     "/link",
     async ({ body: { userId, roleId }, _userId }) => {
       //送信者のロールレベルが足りるか確認
@@ -190,4 +232,27 @@ export const role = new Elysia({ prefix: "/role" })
         tags: ["Role"],
       },
     },
-  );
+  )
+  .get(
+    "/list",
+    async ({ error }) => {
+      const roles = await db.roleInfo.findMany();
+
+      //ロールが存在しない
+      if (roles === null) {
+        throw error(404, "Roles not found");
+      }
+
+      return {
+        success: true,
+        message: "Role list",
+        data: roles,
+      };
+    },
+    {
+      detail: {
+        description: "ロール情報の一覧を取得します",
+        tags: ["Role"],
+      },
+    },
+  );;
