df494d4db59fa7e166fa8a86f13b3e0a1306c27e
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-22 08:27:53 +0900

    [change] - 変数名変更

--- src/components/Message/message.module.ts
+++ src/components/Message/message.module.ts
@@ -195,7 +195,7 @@ export const message = new Elysia({ prefix: "/message" })
     "/read-time/update",
     async ({ _userId, body: { channelId, readTime }, server }) => {
       //既読時間を取得して更新する必要があるか調べる
-      const readTimeNow = await db.messageReadTime.findUnique({
+      const readTimeSaved = await db.messageReadTime.findUnique({
         where: {
           channelId_userId: {
             channelId,
@@ -204,8 +204,8 @@ export const message = new Elysia({ prefix: "/message" })
         },
       });
       if (
-        readTimeNow !== null &&
-        readTimeNow.readTime.valueOf() > readTime.valueOf()
+        readTimeSaved !== null &&
+        readTimeSaved.readTime.valueOf() > readTime.valueOf()
       ) {
         throw error(400, "Read time is already newer");
       }
