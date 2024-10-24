ea92ff3124ee75c0ad7e1662fa9c923ce3b198ac
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-10-24 22:11:11 +0900

    [add] - メッセージ検索にロードインデックス指定の追加

--- src/components/Message/message.module.ts
+++ src/components/Message/message.module.ts
@@ -209,11 +209,13 @@ export const message = new Elysia({ prefix: "/message" })
   )
   .get(
     "/search",
-    async ({ query: {content, channelId, userId, hasUrlPreview} }) => {
+    async ({ query: {content, channelId, userId, hasUrlPreview}, params: { loadIndex } }) => {
       //もし検索条件がないならエラー
       if (content === undefined && channelId === undefined && userId === undefined && hasUrlPreview === undefined) {
         throw error(400, "No search condition");
       }
+
+      const messageSkipping = loadIndex ? (loadIndex - 1) * 50 : 0; 
 
       //メッセージを検索する
       const messages = await db.message.findMany({
@@ -226,7 +228,9 @@ export const message = new Elysia({ prefix: "/message" })
         },
         include: {
           MessageUrlPreview: hasUrlPreview ? true : undefined,
-        }
+        },
+        take: 50,
+        skip: messageSkipping,
       });
 
       return {
@@ -240,6 +244,7 @@ export const message = new Elysia({ prefix: "/message" })
         channelId: t.Optional(t.String({ minLength: 1 })),
         userId: t.Optional(t.String({ minLength: 1 })),
         hasUrlPreview: t.Optional(t.Boolean()),
+        loadIndex: t.Optional(t.Number({ minimum: 1, default: 1 })),
       }),
     }
   )
