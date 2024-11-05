cefe4760278322144eb4f2d3a28f14518764be89
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-11-05 14:07:28 +0900

    [change - 返り値省略

--- src/Utils/GetUserViewableChannel.ts
+++ src/Utils/GetUserViewableChannel.ts
@@ -1,4 +1,4 @@
-import { type Channel, PrismaClient } from "@prisma/client";
+import {type Channel, PrismaClient} from "@prisma/client";
 
 /**
  * 指定のユーザーが閲覧できるチャンネル情報を取得する
@@ -26,7 +26,7 @@ export default async function GetUserViewableChannel(
   const userRoleIds = userRolesLinks.map((role) => role.roleId);
 
   //このユーザーが見れるチャンネルIdを取得
-  const viewableChannel = (await db.channel.findMany({
+  return (await db.channel.findMany({
     where: {
       AND: [
         {
@@ -62,16 +62,14 @@ export default async function GetUserViewableChannel(
         },
         !_onlyJoinedChannel //参加しているチャンネルのみを取得する場合はチャンネルに参加しているか確認
           ? {
-              ChannelJoin: {
-                some: {
-                  userId: _userId,
-                },
+            ChannelJoin: {
+              some: {
+                userId: _userId,
               },
-            }
+            },
+          }
           : {},
       ],
     },
   })) as Channel[];
-
-  return viewableChannel;
 }
