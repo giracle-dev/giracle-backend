5ed559a30e494f9870a759a684cbcc7412dc1c51
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-12-05 01:23:18 +0900

    [add] - システムメッセ送信処理での例外処理

--- src/Utils/SendSystemMessage.ts
+++ src/Utils/SendSystemMessage.ts
@@ -15,30 +15,34 @@ export default async function SendSystemMessage(
   _messageTerm: TSystemMessageTerm,
   _server: Server | null = null,
 ) {
-  //メッセージ内容とするJSON
-  const contentJson = {
-    targetUserId: _targetUserId,
-    messageTerm: _messageTerm,
-  };
-  //DBに記録、JSONは文字列化して保存
-  const msg = await db.message.create({
-    data: {
-      channelId: _channelId,
-      userId: "SYSTEM",
-      isSystemMessage: true,
-      content: JSON.stringify(contentJson),
-    },
-  });
+  try {
+    //メッセージ内容とするJSON
+    const contentJson = {
+      targetUserId: _targetUserId,
+      messageTerm: _messageTerm,
+    };
+    //DBに記録、JSONは文字列化して保存
+    const msg = await db.message.create({
+      data: {
+        channelId: _channelId,
+        userId: "SYSTEM",
+        isSystemMessage: true,
+        content: JSON.stringify(contentJson),
+      },
+    });
 
-  //Serverインスタンスが渡されて有効ならWSで通知
-  if (_server) {
-    _server.publish(
-      `channel::${_channelId}`,
-      JSON.stringify({
-        signal: "message::SendMessage",
-        data: msg,
-      }),
-    );
+    //Serverインスタンスが渡されて有効ならWSで通知
+    if (_server) {
+      _server.publish(
+        `channel::${_channelId}`,
+        JSON.stringify({
+          signal: "message::SendMessage",
+          data: msg,
+        }),
+      );
+    }
+  } catch(e) {
+    console.error("SendSystemMessage :: エラー->", e);
   }
 }
 
