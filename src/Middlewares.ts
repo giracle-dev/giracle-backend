94721833f06b4232c9c9c6ac7e9614d350b0e93f
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2025-05-29 16:07:13 +0900

    [change] - ロール権限確認処理のマクロの書き方更新

--- src/Middlewares.ts
+++ src/Middlewares.ts
@@ -43,41 +43,44 @@ const CheckToken = new Elysia({ name: "CheckToken" })
 
 const checkRoleTerm = new Elysia({ name: "checkRoleTerm" })
   .use(CheckToken)
-  .macro(({ onBeforeHandle }) => ({
-    async checkRoleTerm(roleTerm: string) {
-      onBeforeHandle(async ({ _userId }) => {
-        //console.log("Middlewares :: checkRoleTerm : 送信者のユーザーId->", _userId);
+  //.macro(({ onBeforeHandle }) => ({
+  .macro({
+    checkRoleTerm(roleTerm: string) {
+      return {
+        async beforeHandle({ _userId }) {
+          //console.log("Middlewares :: checkRoleTerm : 送信者のユーザーId->", _userId);
 
-        //管理者権限を持つユーザーなら問答無用で通す
-        const isAdmin = await db.roleLink.findFirst({
-          where: {
-            userId: _userId,
-            role: {
-              manageServer: true,
+          //管理者権限を持つユーザーなら問答無用で通す
+          const isAdmin = await db.roleLink.findFirst({
+            where: {
+              userId: _userId,
+              role: {
+                manageServer: true,
+              },
             },
-          },
-        });
-        if (isAdmin !== null) {
-          return;
-        }
+          });
+          if (isAdmin !== null) {
+            return;
+          }
 
-        //該当権限を持つロール付与情報を検索
-        const roleLink = await db.roleLink.findFirst({
-          where: {
-            userId: _userId,
-            role: {
-              [roleTerm]: true,
+          //該当権限を持つロール付与情報を検索
+          const roleLink = await db.roleLink.findFirst({
+            where: {
+              userId: _userId,
+              role: {
+                [roleTerm]: true,
+              },
             },
-          },
-        });
+          });
 
-        //該当権限を持つロール付与情報が無いなら停止
-        if (roleLink === null) {
-          return status(401, "Role level not enough");
-        }
-      });
+          //該当権限を持つロール付与情報が無いなら停止
+          if (roleLink === null) {
+            return status(401, "Role level not enough");
+          }
+        },
+      };
     },
-  }));
+  });
 
 //URLプレビュー生成
 const urlPreviewControl = new Elysia({ name: "urlPreviewControl" })
