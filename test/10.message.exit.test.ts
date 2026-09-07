1f6a7c7856b561d74d4152245e0997d3b56c449d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-09-08 01:44:27 +0900

    remove - 不要なimport

--- test/10.message.exit.test.ts
+++ test/10.message.exit.test.ts
@@ -1,4 +1,4 @@
-import { beforeAll, describe, expect, it, mock } from "bun:test";
+import { beforeAll, describe, expect, it } from "bun:test";
 import { FETCH, INIT } from "./util";
 
 beforeAll(async () => {
