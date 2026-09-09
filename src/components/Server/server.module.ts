9f2eb2408f89547cc9e2262809d3890c17155d05
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-09-10 00:07:11 +0900

    change - Bot更新時の名前入力にバリデーション

--- src/components/Server/server.module.ts
+++ src/components/Server/server.module.ts
@@ -155,7 +155,7 @@ export const server = new Elysia({ prefix: "/server" })
     {
       body: t.Object({
         botId: t.String(),
-        name: t.String(),
+        name: t.String({ minLength: 1, maxLength: 64 }),
         canFetchUserinfo: t.Optional(t.Boolean()),
         canFetchRoleinfo: t.Optional(t.Boolean()),
         canManageUser: t.Optional(t.Boolean()),
