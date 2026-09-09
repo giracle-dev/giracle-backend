78e63f5517925b6e864f053620cf325f37cc753d
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-09-10 00:34:19 +0900

    change - チャンネル閲覧可能確認スクリプトの可読性改善

--- src/Utils/CheckChannelVisibility.ts
+++ src/Utils/CheckChannelVisibility.ts
@@ -21,52 +21,42 @@ export default async function CheckChannelVisibility(
     .select({ roleId: channelViewableRoles.roleId })
     .from(channelViewableRoles)
     .where(eq(channelViewableRoles.channelId, _channelId));
+  if (roleViewable.length === 0) return true;
 
-  if (roleViewable.length > 0) {
-    // チャンネルに参加しているか調べる
-    const channelJoined = await db.query.channelJoins.findFirst({
-      where: and(
-        eq(channelJoins.userId, _userId),
-        eq(channelJoins.channelId, _channelId),
+  // チャンネルに参加しているか調べる
+  const channelJoined = await db.query.channelJoins.findFirst({
+    where: and(
+      eq(channelJoins.userId, _userId),
+      eq(channelJoins.channelId, _channelId),
+    ),
+  });
+  if (channelJoined !== undefined) return true;
+
+  // チャンネルに参加していないならロールで調べる
+  const hasViewableRole = await db.query.roleLinks.findFirst({
+    where: and(
+      eq(roleLinks.userId, _userId),
+      inArray(
+        roleLinks.roleId,
+        roleViewable.map((role) => role.roleId),
       ),
-    });
+    ),
+  });
+  if (hasViewableRole) {
+    return true;
+  }
 
-    // チャンネルに参加していないならロールで調べる
-    if (!channelJoined) {
-      const hasViewableRole = await db.query.roleLinks.findFirst({
-        where: and(
-          eq(roleLinks.userId, _userId),
-          inArray(
-            roleLinks.roleId,
-            roleViewable.map((role) => role.roleId),
-          ),
-        ),
-      });
+  // サーバー管理者の場合は閲覧可能
+  const userAdminRole = db
+    .select({ userId: roleLinks.userId })
+    .from(roleLinks)
+    .innerJoin(roleInfos, eq(roleLinks.roleId, roleInfos.id))
+    .where(
+      and(eq(roleLinks.userId, _userId), eq(roleInfos.manageServer, true)),
+    )
+    .get();
 
-      // ロールを持っていれば閲覧可能
-      if (hasViewableRole) {
-        return true;
-      }
-
-      // サーバー管理者の場合は閲覧可能
-      const userAdminRole = await db
-        .select({ userId: roleLinks.userId })
-        .from(roleLinks)
-        .innerJoin(roleInfos, eq(roleLinks.roleId, roleInfos.id))
-        .where(
-          and(eq(roleLinks.userId, _userId), eq(roleInfos.manageServer, true)),
-        )
-        .get();
-
-      if (userAdminRole) {
-        return true;
-      }
-    } else {
-      // チャンネルに参加している場合はそのまま返す
-      return true;
-    }
-  } else {
-    // 閲覧制限がない場合はそのまま返す
+  if (userAdminRole) {
     return true;
   }
 
